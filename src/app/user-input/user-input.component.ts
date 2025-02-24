5bfaf873d23d46abeccafe9cd4e6b26b3130d1a1
ddang8-jpeg <parkrangerdang@gmail.com>
2025-02-24 17:19:15 -0500

    Implemented user-input component.

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { calculateInvestmentService } from '../investment-results/investment-results.service';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  private calculateInvestmentService = inject(calculateInvestmentService);
 
+  initialInvestments: number = 0;
+  annualInvestments: number = 0;
+  expectedReturns: number = 0;
+  duration: number = 0;
+
+  onSubmit() {
+    this.calculateInvestmentService.calculateInvestmentResults({
+      initialInvestment: this.initialInvestments,
+      annualInvestment: this.annualInvestments,
+      expectedReturn: this.expectedReturns,
+      duration: this.duration,
+    });
+  }
 }
