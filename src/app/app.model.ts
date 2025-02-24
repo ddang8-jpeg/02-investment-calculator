8e5250ceb55c9838071a5f98b333e8b5b65d7e80
ddang8-jpeg <parkrangerdang@gmail.com>
2025-02-24 17:18:46 -0500

    Added Types for input data and annual data elements.

---
+++ src/app/app.model.ts
@@ -0,0 +1,14 @@
+export type inputData = {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+};
+export type annualDataElement = {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+};
